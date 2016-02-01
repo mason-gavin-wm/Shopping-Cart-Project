e65f7e19ad196e26d80ad7e5acc26f57dd5e647d
Gavin Mason <session1@START-Coding-MBP-15.local>
2016-02-01 13:18:40 -0700

    First Commit Basic Home Page

---
+++ home.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: session1
+ * Date: 1/28/16
+ * Time: 1:40 PM
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Apple Cycle</title>
+    <link rel="stylesheet" type="text/css" href="homestyle.css">
+    <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+
+    <div id="container">
+
+            <div id="H-navigation">
+
+    </div>
+
+</div>
+
+            <div id="jumbo-slide">
+
+</div>
+
+
+<div id="buttonz-slide">
+
+</div>
+
+
+    <div id="content">
+
+        <div id="pic-1">
+
+    </div>
+
+        <div id="pic-2">
+
+    </div>
+
+        <div id="pic-3">
+
+    </div>
+
+        <div id="pic-4">
+
+    </div>
+
+    </div>
+
+
+</body>
+</html>
