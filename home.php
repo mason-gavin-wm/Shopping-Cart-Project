37680f2515b9d23158630b9a6cbb134d9f650ff6
Gavin Mason <session1@START-Coding-MBP-15.local>
2016-02-02 15:26:39 -0700

    Made a  functional drop down navigation bar.

--- home.php
+++ home.php
@@ -9,7 +9,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Apple Cycle</title>
+    <title>Techno Now</title>
     <link rel="stylesheet" type="text/css" href="homestyle.css">
     <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
 </head>
@@ -18,34 +18,37 @@
 
     <div id="container">
 
-            <div id="H-navigation">
+        <div id="H-navigation">
+            <nav>
+
 
                 <ul>
 
-                    <li>
-                        <a href="#">
-                            <img src="http://cdn.osxdaily.com/wp-content/uploads/2010/10/giant-apple-logo-bw.png" style="width:1.2%;height:1.2%;margin:1% 2% auto;">
-                        </a>
-                    </li>
-                    <li>
-                        <a href="products.php">Products</a>
-                    </li>
-                    <li>
-                        <a href="shopping_cart.php">Shopping Cart</a>
-                    </li>
-                    <li>
-                        <a href="checkout.php">Checkout</a>
-                    </li>
-                    <li>
-                        <a href="personal_profile.php">Personal Profile</a>
+
+                    <li><a href="#"><img src="http://www.clker.com/cliparts/i/s/H/f/4/T/apple-logo-white.svg" style="width:30px;height:30px;"></a></li>
+
+                    <li class="hover"><a href="products.php">Products</a>
+                        <ul>
+                            <li><a href="#">link</a></li>
+                            <li><a href="#">link</a></li>
+                            <li><a href="#">link</a></li>
+                        </ul>
                     </li>
 
+                    <li class="hover"><a href="shopping_cart.php">Shopping Cart</a>
 
+                    </li>
+
+                    <li class="hover"><a href="personal_profile.php">Profile</a>
+                        <ul>
+                            <li><a href="#">link</a></li>
+                        </ul>
+                    </li>
 
                 </ul>
 
-
-            </div>
+            </nav>
+        </div>
 
             <div id="jumbo-slide">
 
