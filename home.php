e62acd70fec5f7a90c6d1fcd0eba6651b56a0a8f
Gavin Mason <session1@START-Coding-MBP-15.local>
2016-02-05 14:59:15 -0700

    Created iPhone iPad and watch paged linked to everything, linked all the hoe and shopping cart pages through the website. As well added a shopping cart icon and deleted the checkout navigation from top.

--- home.php
+++ home.php
@@ -23,31 +23,31 @@
         <div id="H-navigation">
             <nav>
 
-
                 <ul>
+                    
+                    <li><a href="home.php"><img src="http://www.clker.com/cliparts/i/s/H/f/4/T/apple-logo-white.svg" style="width:35px;height:35px;position: absolute"></a></li>
 
+                    <li>
+                        <div id="circle" style="border-radius:25px; background-color: white;position: absolute;width:37px;height:37px; margin-top:5px;">
+                            <a href="shopping_cart.php"><img src="http://cdn.flaticon.com/png/256/2772.png" style="width:27px; height:27px;margin-left:4px;margin-top:1px;"></a>
+                        </div>
+                    </li>
 
-                    <li><a href="#"><img src="http://www.clker.com/cliparts/i/s/H/f/4/T/apple-logo-white.svg" style="width:30px;height:30px;"></a></li>
-
-                    <li class="hover"><a href="products.php">Products</a>
+                    <li class="hover"><a href="products.php" >Products</a>
                         <ul>
-                            <li><a href="#">link</a></li>
-                            <li><a href="#">link</a></li>
-                            <li><a href="#">link</a></li>
+                            <li><a href="iPad.php">iPad</a></li>
+                            <li><a href="iPhone.php">iPhone</a></li>
+                            <li><a href="watch.php">Watch</a></li>
                         </ul>
                     </li>
 
-                    <li class="hover"><a href="personal_profile.php">Profile</a>
-                        <ul>
-                            <li><a href="#">link</a></li>
-                        </ul>
-                    </li>
+
+                    <li class="hover"><a href="personal_profile.php" style="margin-left:1100px;position: absolute;">Profile</a></li>
 
                 </ul>
 
             </nav>
         </div>
-
 
 
                 <div id="quickslider">
@@ -73,8 +73,6 @@
 
 
 
-
-
 <div id="buttonz-slide">
 
 </div>
@@ -85,6 +83,8 @@
         <div id="pic-1">
 
             <span>News</span>
+            <p>News about the new products</p>
+
 
 
         </div>
@@ -92,6 +92,7 @@
         <div id="pic-2">
 
             <span>About</span>
+            <p>About the company</p>
 
 
         </div>
@@ -99,19 +100,26 @@
         <div id="pic-3">
 
             <span>Contact</span>
-
-
-        </div>
-
-        <div id="pic-4">
-
-            <div id="FQ">Frequent Questions</div>
+            <p>Contact info about the company</p>
 
 
         </div>
 
 
     </div>
+
+        <div id="content-more-info">
+
+
+
+
+
+
+
+
+
+
+        </div>
 
 </div>
 
---
+++ watch.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: session1
+ * Date: 2/5/16
+ * Time: 2:45 PM
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Watch's</title>
+    <link rel="stylesheet" type="text/css" href="homestyle.css">
+    <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+    <script src="jquery copy.js"></script>
+    <script src="java.js"></script>
+</head>
+
+<body>
+
+<div id="container">
+
+    <div id="H-navigation">
+        <nav>
+
+
+            <ul>
+
+
+                <li><a href="home.php"><img src="http://www.clker.com/cliparts/i/s/H/f/4/T/apple-logo-white.svg" style="width:35px;height:35px;position: absolute"></a></li>
+
+                <li>
+                    <div id="circle" style="border-radius:25px; background-color: white;position: absolute;width:37px;height:37px; margin-top:5px;">
+                        <a href="shopping_cart.php"><img src="http://cdn.flaticon.com/png/256/2772.png" style="width:27px; height:27px;margin-left:4px;margin-top:1px;"></a>
+                    </div>
+                </li>
+
+                <li class="hover"><a href="products.php" >Products</a>
+                    <ul>
+                        <li><a href="iPad.php">iPad</a></li>
+                        <li><a href="iPhone.php">iPhone</a></li>
+                        <li><a href="watch.php">Watch</a></li>
+                    </ul>
+                </li>
+
+
+                <li class="hover"><a href="personal_profile.php" style="margin-left:1100px;position: absolute;">Profile</a></li>
+
+            </ul>
+
+        </nav>
+    </div>
+
+
+
+
+
+
+
+
+
+    <div id="buttonz-slide">
+
+    </div>
+
+
+    <div id="content-pic">
+
+        <div id="pic-1">
+
+            <span>News</span>
+            <p>News about the watch</p>
+
+
+        </div>
+
+        <div id="pic-2">
+
+            <span>About</span>
+            <p>About the watch</p>
+
+
+        </div>
+
+        <div id="pic-3">
+
+            <span>Contact</span>
+            <p>If watch is broken call this</p>
+
+
+        </div>
+
+
+    </div>
+
+</div>
+
+
+</body>
+</html>
