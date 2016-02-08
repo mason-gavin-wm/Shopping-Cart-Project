b74089b7b4594a5f319327b8bee5777743650449
Gavin Mason <session1@START-Coding-MBP-15.local>
2016-02-08 15:14:03 -0700

    Deleted Slider, made content little on the products page.

--- home.php
+++ home.php
@@ -9,7 +9,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>NAME_OF_SITE</title>
+    <title>iApple</title>
     <link rel="stylesheet" type="text/css" href="homestyle.css">
     <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
     <script src="jquery copy.js"></script>
@@ -18,65 +18,39 @@
 
 <body>
 
-    <div id="container">
+<div id="container">
 
-        <div id="H-navigation">
-            <nav>
+    <div id="H-navigation">
+        <nav>
 
-                <ul>
-                    
-                    <li><a href="home.php"><img src="http://www.clker.com/cliparts/i/s/H/f/4/T/apple-logo-white.svg" style="width:35px;height:35px;position: absolute"></a></li>
+            <ul>
 
-                    <li>
-                        <div id="circle" style="border-radius:25px; background-color: white;position: absolute;width:37px;height:37px; margin-top:5px;">
-                            <a href="shopping_cart.php"><img src="http://cdn.flaticon.com/png/256/2772.png" style="width:27px; height:27px;margin-left:4px;margin-top:1px;"></a>
-                        </div>
-                    </li>
+                <li><a href="home.php"><img src="http://www.clker.com/cliparts/i/s/H/f/4/T/apple-logo-white.svg" style="width:35px;height:35px;position: absolute"></a></li>
 
-                    <li class="hover"><a href="products.php" >Products</a>
-                        <ul>
-                            <li><a href="iPad.php">iPad</a></li>
-                            <li><a href="iPhone.php">iPhone</a></li>
-                            <li><a href="watch.php">Watch</a></li>
-                        </ul>
-                    </li>
-
-
-                    <li class="hover"><a href="personal_profile.php" style="margin-left:1100px;position: absolute;">Profile</a></li>
-
-                </ul>
-
-            </nav>
-        </div>
-
-
-                <div id="quickslider">
-                    <div class="quickslider" >
-                        <img  id="1" src="http://www.i-teach.org.uk/uploads/iPad-Air-The-Power-Of-Lightness.png">
-                        <img  id="2" src="http://www.iphone6updates.us/wp-content/uploads/2013/04/Apple-iPhone-6-Concept-45.jpg">
-                        <img  id="3" src="http://wallpapershome.com/images/wallpapers/apple-watch-3840x2160-watches-wallpaper-5k-4k-review-iwatch-apple-3515.jpg">
-                    </div><!--quickslider-->
-
-                    <div class="nav-thumbs">
-                        <ul>
-                            <li><a href="#" class="1">1</a></li>
-                            <li><a href="#" class="2">2</a></li>
-                            <li><a href="#" class="3">3</a></li>
-                        </ul>
+                <li>
+                    <div id="circle" style="border-radius:25px; background-color: white;position: absolute;width:37px;height:37px; margin-top:5px;">
+                        <a href="shopping_cart.php"><img src="http://cdn.flaticon.com/png/256/2772.png" style="width:27px; height:27px;margin-left:4px;margin-top:1px;"></a>
                     </div>
+                </li>
 
-                    <div class="quickslider-nav" >
-                        <a href="#" class="left">Prev</a>
-                        <a href="#" class="right" onclick="next(); return false;">Next</a>
-                    </div>
-                </div><!--quickslider-->
+                <li class="hover"><a href="products.php" >Products</a>
+                    <ul>
+                        <li><a href="iPad.php">iPad</a></li>
+                        <li><a href="iPhone.php">iPhone</a></li>
+                        <li><a href="watch.php">Watch</a></li>
+                    </ul>
+                </li>
 
 
+                <li class="hover"><a href="personal_profile.php" style="margin-left:1100px;position: absolute;">Profile</a></li>
 
-<div id="buttonz-slide">
+            </ul>
 
-</div>
+        </nav>
+    </div>
 
+
+    
 
     <div id="content-pic">
 
@@ -108,7 +82,9 @@
 
     </div>
 
-        <div id="content-more-info">
+    <div id="content-more-info">
+
+        <h2></h2>
 
 
 
@@ -117,9 +93,7 @@
 
 
 
-
-
-        </div>
+    </div>
 
 </div>
 
--- products.php
+++ products.php
@@ -9,15 +9,98 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Apple Cycle</title>
+    <title>iApple</title>
     <link rel="stylesheet" type="text/css" href="homestyle.css">
     <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+    <script src="jquery copy.js"></script>
+    <script src="java.js"></script>
 </head>
 
 <body>
 
+<div id="container">
 
-<p>This is the Product Page</p>
+    <div id="H-navigation">
+        <nav>
+
+            <ul>
+
+                <li><a href="home.php"><img src="http://www.clker.com/cliparts/i/s/H/f/4/T/apple-logo-white.svg" style="width:35px;height:35px;position: absolute"></a></li>
+
+                <li>
+                    <div id="circle" style="border-radius:25px; background-color: white;position: absolute;width:37px;height:37px; margin-top:5px;">
+                        <a href="shopping_cart.php"><img src="http://cdn.flaticon.com/png/256/2772.png" style="width:27px; height:27px;margin-left:4px;margin-top:1px;"></a>
+                    </div>
+                </li>
+
+                <li class="hover"><a href="products.php" >Products</a>
+                    <ul>
+                        <li><a href="iPad.php">iPad</a></li>
+                        <li><a href="iPhone.php">iPhone</a></li>
+                        <li><a href="watch.php">Watch</a></li>
+                    </ul>
+                </li>
+
+
+                <li class="hover"><a href="personal_profile.php" style="margin-left:1100px;position: absolute;">Profile</a></li>
+
+            </ul>
+
+        </nav>
+    </div>
+   
+
+<h1 style="margin-top:100px;margin-left:45%;position:absolute;text-decoration: underline">THESE PRODUCTS ARE THE BEST AROUND</h1>
+    <br><br>
+    <p class="text" style="margin-top:175px;">THIS IS BLAH BLAH BLAH BLAH  BKLABHA KBLBLAJB K AJKBAKJD KA K THIS IS BLAH BLAH BLAH BLAH  BKLABHA KBLBLAJB K AJKBAKJD KA K THIS IS BLAH BLAH BLAH BLAH  BKLABHA KBLBLAJB K AJKBAKJD KA K</p>
+    <p class="text" style="margin-top:310px;">THIS IS BLAH BLAH BLAH BLAH  BKLABHA KBLBLAJB K AJKBAKJD KA K THIS IS BLAH BLAH BLAH BLAH  BKLABHA KBLBLAJB K AJKBAKJD KA K THIS IS BLAH BLAH BLAH BLAH  BKLABHA KBLBLAJB K AJKBAKJD KA K</p>
+    <p class="text" style="margin-top:450px;">THIS IS BLAH BLAH BLAH BLAH  BKLABHA KBLBLAJB K AJKBAKJD KA K THIS IS BLAH BLAH BLAH BLAH  BKLABHA KBLBLAJB K AJKBAKJD KA K THIS IS BLAH BLAH BLAH BLAH  BKLABHA KBLBLAJB K AJKBAKJD KA K</p>
+
+    <div id="content-pic">
+
+        <div id="pic-1">
+
+            <span>iPad's</span>
+            <img src="https://wallpaperscraft.com/image/ipad_pro_apple_ipad_104327_3840x2400.jpg" style="position:absolute;width: 50.3%;height: 180px;">
+            <br>iPad's are even cooler than them all!
+
+
+
+        </div>
+
+        <div id="pic-2">
+
+            <span>iPhone's</span>
+            <img src="http://cdn2.macworld.co.uk/cmsdata/reviews/3623611/iphone_6s_review_60.jpg" style="position:absolute;width: 50.3%;height: 180px;">
+            <p>iPhone's are the best!</p>
+
+
+        </div>
+
+        <div id="pic-3">
+
+            <span>Apple Watch's</span>
+            <img src="https://i.ytimg.com/vi/9T4-efI_7L0/maxresdefault.jpg" style="position:absolute;width: 35.3%;height: 180px;">
+            <p>Apple Watch's are cool!</p>
+
+        </div>
+
+    </div>
+
+    <div id="content-more-info">
+
+        <h2></h2>
+
+
+
+
+
+
+
+
+    </div>
+
+</div>
 
 
 </body>
